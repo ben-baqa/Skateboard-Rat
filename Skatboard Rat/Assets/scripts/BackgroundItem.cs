346dc64fb30672bf5379980c48e868186fb32938
ben-baqa <56326234+ben-baqa@users.noreply.github.com>
2020-05-16 18:25:54 -0600

    Added Background Objects

---
+++ Skatboard Rat/Assets/scripts/BackgroundItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundItem : MonoBehaviour
+{
+    public float speedMultiplier;
+
+    private Transform tr;
+    private RatBehavior rat;
+    
+    void Start()
+    {
+        rat = GameObject.Find("Rat(Clone)").GetComponent<RatBehavior>();
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (tr.position.x < -40)
+        {
+            Destroy(gameObject);
+        }
+        if (rat == null)
+        {
+            rat = GameObject.Find("Rat(Clone)").GetComponent<RatBehavior>();
+        }
+        tr.position = new Vector2(tr.position.x - rat.speed * speedMultiplier, tr.position.y);
+    }
+}
---
+++ Skatboard Rat/Assets/scripts/BackgroundSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSpawner : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public float delay;
+    public int obstaclesLength;
+
+    private RatBehavior rat;
+    private float timer;
+
+    void Start()
+    {
+        //rat = GameObject.Find("Rat(Clone)").GetComponent<RatBehavior>();
+        timer = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rat == null)
+        {
+            rat = GameObject.Find("Rat(Clone)").GetComponent<RatBehavior>();
+        }
+        if (timer > delay / (0.0001 + Mathf.Pow(rat.speed, 4)))
+        {
+            if ((int)Random.Range(0, 100) == 8)
+            {
+                SpawnObject((int)Random.Range(0, obstacles.Length));
+                timer = 0;
+            }
+        }
+        timer++;
+    }
+
+    private void SpawnObject(int n)
+    {
+        GameObject inst = Instantiate(obstacles[n]);
+        inst.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0.5f, 1), Random.Range(0.5f, 1), Random.Range(0.5f, 1));
+    }
+}
